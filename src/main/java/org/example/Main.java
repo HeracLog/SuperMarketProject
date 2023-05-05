a67abc11b631a521e0b72af8b639d8ecfcb452d3
YoussefAdel26 <yousseg.adel26@gmail.com>
2023-05-05 15:36:20 +0300

    Youssef Adel Mohamed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Date date = new Date();
+        System.out.println(date.getMonth());
     }
 }
